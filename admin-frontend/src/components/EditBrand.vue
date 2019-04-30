2308283c1c2234bda78d091ce805c647a8b97dee
Unknown <ketsara.y@mail.kmutt.ac.th>
2019-04-30 14:30:02 +0700

    [ADD] EditBrand

---
+++ admin-frontend/src/components/EditBrand.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-layout>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn icon v-on="on">
+          <v-img :src="require('../assets/edit.png')"/>
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Add Brand</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12>
+                <v-text-field label="Name*" required></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions style="margin: 0 187px 0 187px">
+          <v-btn color="blue darken-1"  @click="dialog = false">Close</v-btn>
+          <div @click="dialog = false" style="margin: 30px"><v-btn color="blue darken-1"  @click="onEditClick">Save</v-btn></div>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-layout>
+</template>
+
+<script>
+import Swal from 'sweetalert2'
+
+export default {
+  methods: {
+      onEditClick: function (e) {
+      Swal.fire({
+        position: 'center',
+        type: 'success',
+        title: 'Your work has been saved',
+        showConfirmButton: false,
+        timer: 1000
+})
+      }
+    },
+  data: () => ({
+    dialog: false
+  })
+};
+</script>
