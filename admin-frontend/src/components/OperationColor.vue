f593160b4c3f59d6284ae80b7c09978c9ec97889
Unknown <ketsara.y@mail.kmutt.ac.th>
2019-04-30 14:34:11 +0700

    [UPDATE] lipstick color management

---
+++ admin-frontend/src/components/OperationColor.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-toolbar id="tool-bar">
+
+    <v-spacer></v-spacer>
+    
+    
+    <!-- <EditColor/> -->
+    <AddColor/>
+    <ConfirmDelete/>
+
+  </v-toolbar>
+</template>
+
+<script>
+import AddColor from "../components/AddColor";
+// import EditColor from "../components/EditColor";
+import ConfirmDelete from "../components/ConfirmDelete";
+
+export default {
+  components: {
+    AddColor,
+    // EditColor,
+    ConfirmDelete
+  }
+};
+</script>
+
+
+
+
+
+
+
