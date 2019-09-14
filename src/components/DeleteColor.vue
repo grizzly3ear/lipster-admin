5bfc5a868e7e408ff0bbbcfa1fbf048d2d5cde82
Unknown <ketsara.y@mail.kmutt.ac.th>
2019-09-14 22:38:48 +0700

    [UPDATE] correct api get detail

--- src/components/DeleteColor.vue
+++ src/components/DeleteColor.vue
@@ -1,64 +1,62 @@
 <template>
-  <v-layout class='operation-icon'>
-    <v-btn  icon @click='onDeleteClick'>
+  <v-layout class="operation-icon">
+    <v-btn icon @click="onDeleteClick">
       <v-icon color="black">delete</v-icon>
     </v-btn>
   </v-layout>
 </template>
 
 <script>
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 import axios from "axios";
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions, mapGetters } from "vuex";
 
-  export default {
-    methods: {
-      ...mapActions([
-      'setColor'
-    ]),
-        onDeleteClick () {
-            Swal.fire({
-            title: 'Are you sure?',
-            text: 'You wont be able to revert this!',
-            type: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#d33',
-            cancelButtonColor: '#3085d6',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-                if (result.value) {  
-                axios.delete(`http://18.136.104.217/api/lipstick/color/` + this.props.item.id)
-                Swal.fire(
-                    'Deleted!',
-                    'This item has been deleted.',
-                    'success'
-                ).catch(error => {
-                        Swal.fire(
-                            'Deleted!',
-                            'This item has been failed to deleted.',
-                            'warning'
-                        )
-                    }).finally(() => {
-                        axios.get(`http://18.136.104.217/api/lipstick/detail/` + this.$route.params.id).then(({ data }) => {
-                            this.setColor(data)
-                        })
-                    })
-                }
-            }) 
-        },
-    },
-    props: [
-        'props'
-        ],
-    data () {
-      return {
-        
-      }
-    },
-    computed: {
-      ...mapGetters([
-        'getColor'
-      ])
+export default {
+  methods: {
+    ...mapActions(["setColor"]),
+    onDeleteClick() {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You wont be able to revert this!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3085d6",
+        confirmButtonText: "Yes, delete it!"
+      }).then(result => {
+        if (result.value) {
+          axios.delete(
+            `http://18.136.104.217/api/lipstick/color/` + this.props.item.id
+          );
+          Swal.fire("Deleted!", "This item has been deleted.", "success")
+            .catch(error => {
+              Swal.fire(
+                "Deleted!",
+                "This item has been failed to deleted.",
+                "warning"
+              );
+            })
+            .finally(() => {
+              axios
+                .get(
+                  `http://18.136.104.217/api/lipstick/detail/` +
+                    this.$route.params.id +
+                    `?part=color`
+                )
+                .then(({ data }) => {
+                  this.setColor(data);
+                });
+            });
+        }
+      });
     }
+  },
+  props: ["props"],
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(["getColor"])
   }
+};
 </script>
