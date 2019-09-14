3088a15d100a55a00a17ea650c8c2f85821e11ff
Unknown <ketsara.y@mail.kmutt.ac.th>
2019-09-14 15:43:23 +0700

    [UPDATE] correct post color

--- src/components/AddColor.vue
+++ src/components/AddColor.vue
@@ -1,37 +1,39 @@
 <template>
   <v-layout>
-    <v-dialog v-model='dialog' persistent max-width='600px'>
-      <template v-slot:activator='{ on }'>
-        <v-btn icon v-on='on'>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn icon v-on="on">
           <v-icon color="black">add_box</v-icon>
         </v-btn>
       </template>
       <v-card>
         <v-card-title>
-          <span class='headline'>Add Lipstick Color</span>
+          <span class="headline">Add Lipstick Color</span>
         </v-card-title>
         <v-card-text>
           <v-container grid-list-md>
             <v-layout wrap>
               <v-flex xs12 sm6 md4>
-                <v-text-field v-model='color_name' label='Color Name*' required></v-text-field>
+                <v-text-field v-model="color_name" label="Color Name*" required></v-text-field>
               </v-flex>
               <v-flex xs12 sm6 md4>
-                <v-text-field v-model='rgb' label='RGB*' required></v-text-field>
+                <v-text-field v-model="rgb" label="RGB*" required></v-text-field>
               </v-flex>
               <v-flex xs12 sm6 md4>
-                <v-text-field v-model='color_code' label='Color Name*' required></v-text-field>
+                <v-text-field v-model="color_code" label="Color Name*" required></v-text-field>
               </v-flex>
               <v-flex xs12>
-                <v-text-field v-model='image' label='Image*' required></v-text-field>
+                <v-text-field v-model="image" label="Image*" required></v-text-field>
               </v-flex>
             </v-layout>
           </v-container>
           <small>*indicates required field</small>
         </v-card-text>
-        <v-card-actions style='margin: 0 187px 0 187px'>
-          <v-btn color='blue darken-1'  @click='dialog = false'>Close</v-btn>
-          <div @click='dialog = false' style='margin: 30px'><v-btn color='blue darken-1'  @click='onAddClick'>Save</v-btn></div>
+        <v-card-actions style="margin: 0 187px 0 187px">
+          <v-btn color="blue darken-1" @click="dialog = false">Close</v-btn>
+          <div @click="dialog = false" style="margin: 30px">
+            <v-btn color="blue darken-1" @click="onAddClick">Save</v-btn>
+          </div>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -39,48 +41,60 @@
 </template>
 
 <script>
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 import axios from "axios";
-import { mapGetters, mapActions } from 'vuex'
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-    methods: {
-      ...mapActions([
-      'setColor'
-    ]),
+  methods: {
+    ...mapActions(["setColor"]),
     async onAddClick() {
-      let formData = new FormData()
-      formData.append('color_name', this.color_name)
-      formData.append('rgb', this.rgb)
-      formData.append('color_code', this.color_code)
-      formData.append('lipstick_detail_id', this.$route.params.id)
-      await axios.post(`http://18.136.104.217/api/lipstick`, 
-      formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
+      let formData = new FormData();
+      formData.append("color_name", this.color_name);
+      formData.append("rgb", this.rgb);
+      formData.append("color_code", this.color_code);
+      formData.append("lipstick_detail_id", this.$route.params.id);
+      let newColor = await axios.post(
+        `http://18.136.104.217/api/lipstick/color`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data"
+          }
         }
-      })
+      );
+      let formImage = new FormData();
+      formImage.append("image", this.image);
+      formImage.append("lipstick_color_id", newColor.data.id);
+      await axios.post(`http://18.136.104.217/api/lipstick/image`, formImage, {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      });
       Swal.fire({
-        position: 'center',
-        type: 'success',
-        title: 'Your work has been saved',
+        position: "center",
+        type: "success",
+        title: "Your work has been saved",
         showConfirmButton: false,
         timer: 1000
-      })
-      const { data } = await axios.get(`http://18.136.104.217/api/lipstick/detail/` + this.$route.params.id)
-      this.setColor(data)
+      });
+      const { data } = await axios.get(
+        `http://18.136.104.217/api/lipstick/detail/` +
+          this.$route.params.id +
+          `?part=color`
+      );
+      this.setColor(data);
     }
   },
   data: () => ({
     dialog: false,
-    color_name: '',
-    rgb: '',
-    color_code: ''
+    color_name: "",
+    rgb: "",
+    color_code: "",
+    image: ""
   }),
   computed: {
-    ...mapGetters([
-      'getColor'
-    ])
+    ...mapGetters(["getColor"])
   }
-}
+};
 </script>
--- src/components/ColorList.vue
+++ src/components/ColorList.vue
@@ -1,87 +1,88 @@
 <template>
-
-  <v-data-table id='color-list'
-    :headers='headers'
-    :items='getColor.data.colors'
-    item-key='getColor.data.colors.id'
+  <v-data-table
+    id="color-list"
+    :headers="headers"
+    :items="getColor.data.colors"
+    item-key="getColor.data.colors.id"
     select-all
-    class='elevation-1'
+    class="elevation-1"
   >
-    <template v-slot:items='props'>
+    <template v-slot:items="props">
       <td>
-        <SelectLipstickColorCard :props='props'/>
+        <SelectLipstickColorCard :props="props" />
       </td>
       <td>
-        <v-list v-for='image in props.item.images' :key='image.id'> 
-          <img class="image-container" :src="image.image"/>
+        <v-list v-for="image in props.item.images" :key="image.id">
+          <img class="image-container" :src="image.image" />
         </v-list>
       </td>
       <td>{{ props.item.color_name }}</td>
       <td>
-        <div v-html='renderColorBox(props.item.rgb)'></div>
+        <div v-html="renderColorBox(props.item.rgb)"></div>
         {{ props.item.rgb }}
       </td>
-      <td>{{ props.item.color_code }}</td>      
-      <td><EditColor :props='props'/></td>
-      <td><DeleteColor :props='props'/></td>
+      <td>{{ props.item.color_code }}</td>
+      <td>
+        <EditColor :props="props" />
+      </td>
+      <td>
+        <DeleteColor :props="props" />
+      </td>
       <td></td>
     </template>
   </v-data-table>
-
 </template>
 
 <script>
-import EditColor from '@/components/EditColor'
-import DeleteColor from '@/components/DeleteColor'
-import SelectLipstickColorCard from '@/components/SelectLipstickColorCard'
+import EditColor from "@/components/EditColor";
+import DeleteColor from "@/components/DeleteColor";
+import SelectLipstickColorCard from "@/components/SelectLipstickColorCard";
 import axios from "axios";
-import { mapGetters, mapActions } from 'vuex'
+import { mapGetters, mapActions } from "vuex";
 
-  export default {
-    components: {
-      EditColor,
-      DeleteColor,
-      SelectLipstickColorCard
+export default {
+  components: {
+    EditColor,
+    DeleteColor,
+    SelectLipstickColorCard
+  },
+  methods: {
+    ...mapActions(["setColor"]),
+    async getColors() {
+      const { data } = await axios.get(
+        `http://18.136.104.217/api/lipstick/detail/` +
+          this.$route.params.id +
+          `?part=color`
+      );
+      this.colors = data.data;
+      this.setColor(data);
     },
-    methods: {
-      ...mapActions([
-      'setColor'
-    ]),
-      async getColors() {
-        const { data } = await axios.get(`http://18.136.104.217/api/lipstick/detail/` + this.$route.params.id)
-        this.colors = data
-        this.setColor(data)
-      },
-      renderColorBox(rgb) {
-        return (
-          `<div class='color-box' style="background-color: ${rgb}"></div>`
-        )
-      }
-    },
-    async mounted () {
-      this.getColors()
-    },
-    data () {
-      return {
-        selected: [],
-        headers: [
-          { text: 'Image', value: 'image' },
-          { text: 'Color Name', value: 'color_name' },
-          { text: 'RGB', value: 'rgb' },
-          { text: 'Color Code', value: 'color_code' },
-          { text: 'Edit' },
-          { text: 'Delete'},
-          { text: ''}     
-        ],
-        colors: {},
-      }
-    },
+    renderColorBox(rgb) {
+      return `<div class='color-box' style="background-color: ${rgb}"></div>`;
+    }
+  },
+  async mounted() {
+    this.getColors();
+  },
+  data() {
+    return {
+      selected: [],
+      headers: [
+        { text: "Image", value: "image" },
+        { text: "Color Name", value: "color_name" },
+        { text: "RGB", value: "rgb" },
+        { text: "Color Code", value: "color_code" },
+        { text: "Edit" },
+        { text: "Delete" },
+        { text: "" }
+      ],
+      colors: {}
+    };
+  },
   computed: {
-    ...mapGetters([
-      'getColor'
-    ])
+    ...mapGetters(["getColor"])
   }
-  }
+};
 </script>
 
 
