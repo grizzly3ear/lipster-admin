c083d6302e204d64aec40ea4b017ff3071be9869
Unknown <ketsara.y@mail.kmutt.ac.th>
2019-09-14 15:41:46 +0700

    [UPDATE] correct put color and image

--- src/components/EditColor.vue
+++ src/components/EditColor.vue
@@ -1,37 +1,39 @@
 <template>
   <v-layout>
-    <v-dialog v-model='dialog' persistent max-width='600px'>
-      <template v-slot:activator='{ on }'>
-        <v-btn icon v-on='on'>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn icon v-on="on">
           <v-icon color="black">edit</v-icon>
         </v-btn>
       </template>
       <v-card>
         <v-card-title>
-          <span class='headline'>Edit Lipstick Color</span>
+          <span class="headline">Edit Lipstick Color</span>
         </v-card-title>
         <v-card-text>
           <v-container grid-list-md>
             <v-layout wrap>
               <v-flex xs12 sm6 md4>
-                <v-text-field v-model='color_name' label='Color Name*' required></v-text-field>
+                <v-text-field v-model="color_name" label="Color Name*" required></v-text-field>
               </v-flex>
               <v-flex xs12 sm6 md4>
-                <v-text-field v-model='rgb' label='RGB*' required></v-text-field>
+                <v-text-field v-model="rgb" label="RGB*" required></v-text-field>
               </v-flex>
               <v-flex xs12 sm6 md4>
-                <v-text-field v-model='color_code' label='Color Code*' required></v-text-field>
+                <v-text-field v-model="color_code" label="Color Code*" required></v-text-field>
               </v-flex>
               <v-flex xs12>
-                <v-text-field label='Image*' required></v-text-field>
+                <v-text-field v-model="image" label="Image*" required></v-text-field>
               </v-flex>
             </v-layout>
           </v-container>
           <small>*indicates required field</small>
         </v-card-text>
-        <v-card-actions style='margin: 0 187px 0 187px'>
-          <v-btn color='blue darken-1'  @click='dialog = false'>Close</v-btn>
-          <div @click='dialog = false' style='margin: 30px'><v-btn color='blue darken-1'  @click='onEditClick'>Save</v-btn></div>
+        <v-card-actions style="margin: 0 187px 0 187px">
+          <v-btn color="blue darken-1" @click="dialog = false">Close</v-btn>
+          <div @click="dialog = false" style="margin: 30px">
+            <v-btn color="blue darken-1" @click="onEditClick">Save</v-btn>
+          </div>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -39,47 +41,58 @@
 </template>
 
 <script>
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 import axios from "axios";
-import { mapGetters, mapActions } from 'vuex'
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-     methods: {
-       ...mapActions([
-      'setColor'
-      ]),
-    async onEditClick(){
-        await axios.put(`http://18.136.104.217/api/lipstick/color/` + this.props.item.id,
+  methods: {
+    ...mapActions(["setColor"]),
+    async onEditClick() {
+      await axios.put(
+        `http://18.136.104.217/api/lipstick/color/` + this.props.item.id,
         {
-        color_name: this.color_name,
-        rgb: this.rgb,
-        color_code: this.color_code,
-        lipstick_detail_id: this.props.item.lipstick_detail_id
-        })
-        Swal.fire({
-          position: 'center',
-          type: 'success',
-          title: 'Your work has been saved',
-          showConfirmButton: false,
-          timer: 1000
-        })
-        const { data } = await axios.get(`http://18.136.104.217/api/lipstick/detail/` + this.$route.params.id)
-        this.setColor(data)
-      },
+          color_name: this.color_name,
+          rgb: this.rgb,
+          color_code: this.color_code,
+          lipstick_detail_id: this.$route.params.id
+        }
+      );
+      //TODO: loop images
+      await axios.put(
+        `http://18.136.104.217/api/lipstick/image/` +
+          this.props.item.images[0].id,
+        {
+          image: this.image,
+          lipstick_color_id: this.props.item.id
+        }
+      );
+
+      Swal.fire({
+        position: "center",
+        type: "success",
+        title: "Your work has been saved",
+        showConfirmButton: false,
+        timer: 1000
+      });
+      const { data } = await axios.get(
+        `http://18.136.104.217/api/lipstick/detail/` +
+          this.$route.params.id +
+          `?part=color`
+      );
+      this.setColor(data);
+    }
   },
-  props: [
-    'props'
-  ],
+  props: ["props"],
   data: () => ({
     dialog: false,
-    color_name: '',
-    rgb: '',
-    color_code: ''
+    color_name: "",
+    rgb: "",
+    color_code: "",
+    image: ""
   }),
   computed: {
-    ...mapGetters([
-      'getColor'
-    ])
+    ...mapGetters(["getColor"])
   }
-}
+};
 </script>
