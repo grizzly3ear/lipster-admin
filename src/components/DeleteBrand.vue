bb963974bcea2dfdbbdae8361f6df0d5a0395084
Unknown <ketsara.y@mail.kmutt.ac.th>
2019-10-23 17:56:46 +0700

    [UPDATE] correct path

--- src/components/DeleteBrand.vue
+++ src/components/DeleteBrand.vue
@@ -1,64 +1,57 @@
 <template>
-  <v-layout class='operation-icon'>
-    <v-btn icon @click='onDeleteClick'>
+  <v-layout class="operation-icon">
+    <v-btn icon @click="onDeleteClick">
       <v-icon color="black">delete</v-icon>
     </v-btn>
   </v-layout>
 </template>
 
 <script>
-import Swal from 'sweetalert2'
-import axios from '../utils/axios'
-import { mapActions, mapGetters } from 'vuex'
-  
+import Swal from "sweetalert2";
+import axios from "../utils/axios";
+import { mapActions, mapGetters } from "vuex";
+
 export default {
   methods: {
-    ...mapActions([
-      'setBrand'
-    ]),
-      onDeleteClick () {
-        Swal.fire({
-          title: 'Are you sure?',
-          text: 'You wont be able to revert this!',
-          type: 'warning',
-          showCancelButton: true,
-          confirmButtonColor: '#d33',
-          cancelButtonColor: '#3085d6',
-          confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-          if (result.value) {
-            axios.delete(`brand/${this.brand.id}`).then( result => {
-              Swal.fire(
-                'Deleted!',
-                'This item has been deleted.',
-                'success'
-              )
-            }).catch( error => {
-              Swal.fire(
-                'Deleted!',
-                'This item has been failed to deleted.',
-                'warning'
-              )
-            }).finally(() => {
-              axios.get(`brand`).then(({data}) => {
-                this.setBrand(data.data)
-              })
+    ...mapActions(["setBrand"]),
+    onDeleteClick() {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You wont be able to revert this!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3085d6",
+        confirmButtonText: "Yes, delete it!"
+      }).then(result => {
+        if (result.value) {
+          axios
+            .delete(`api/brand/${this.brand.id}`)
+            .then(result => {
+              Swal.fire("Deleted!", "This item has been deleted.", "success");
             })
-          }
-        })
-      },
-    },
-    props: [
-      'brand'
-    ],
-    data () {
-      return {
-      }
-    },
-    computed: {
-      ...mapGetters([
-        'getBrand'
-      ])
+            .catch(error => {
+              Swal.fire(
+                "Deleted!",
+                "This item has been failed to deleted.",
+                "warning"
+              );
+            })
+            .finally(() => {
+              axios.get(`brand`).then(({ data }) => {
+                this.setBrand(data.data);
+              });
+            });
+        }
+      });
     }
+  },
+  props: ["brand"],
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(["getBrand"])
   }
+};
 </script>
